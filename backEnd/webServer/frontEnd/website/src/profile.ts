3fd8126900bd452709d113e6435aca23c310bbe7
dlitran <dariolitran@gmail.com>
2025-07-26 10:29:41 +0200

    profile session changed

--- backEnd/webServer/frontEnd/website/src/profile.ts
+++ backEnd/webServer/frontEnd/website/src/profile.ts
@@ -6,7 +6,51 @@ interface UserProfile {
 	friends: string[];
 	requests: string[];
 	blockedUsers: string[]
-  }
+}
+
+async function getProfile(): Promise<Response>
+{
+
+	const response = await fetch(`https://${window.location.hostname}:8443/usermanagement/front/get/profile_session`, {
+		method: 'GET',
+		credentials: 'include',
+	});
+	if (!response.ok)
+	{
+		console.log("Erroooor!");
+	}
+	let session = await response.json();
+	const id = session.name;
+	console.log("Profile:", session);
+
+	const profileResponse = await fetch(`https://${window.location.hostname}:8443/usermanagement/front/get/profile?id=${id}`, {
+		method: 'GET',
+		credentials: 'include'
+	});
+
+	return(profileResponse);
+}
+
+//let profile: any = getProfile();
+getProfile().then(async profileResponse => {
+	if (profileResponse.ok) {
+		const profile = (await profileResponse.json())[0];
+		console.log(profile.bio);
+	console.log("hola", profile.pseudo);
+	}
+});
+
+//console.log(profile);
+// const response = await fetch(`https://userManagement:3000/usermanagement/front/get/profile_session`, {
+// 	method: 'GET',
+// 	credentials: 'include',
+// });
+// if (!response.ok) {
+// 	console.log("something went wrong with profile_session fetch.");
+// 	window.location.href = "/login";
+// 	return;
+// }
+// const sessionData = await response.json();
 
 //Friend requests
 const requests: string[] = ['Alice', 'Bob', 'Charlie'];
@@ -17,23 +61,23 @@ const templateRequest = document.getElementById('friend-request-template') as HT
 function addRequest(name: string) {
 	if (templateRequest)
 	{
-    	const Clone = templateRequest.content.cloneNode(true) as DocumentFragment;
+		const Clone = templateRequest.content.cloneNode(true) as DocumentFragment;
 
 		const span = Clone.querySelector('span');
 
-        // Check if the span element inside the template exists
-        if (span) {
-            span.textContent = name;
-        }
+		// Check if the span element inside the template exists
+		if (span) {
+			span.textContent = name;
+		}
 
-        if (requestListContainer) {
-            requestListContainer.appendChild(Clone);
+		if (requestListContainer) {
+			requestListContainer.appendChild(Clone);
 		}
 	}
 }
 
 requests.forEach(request => {
-    addRequest(request);
+	addRequest(request);
 });
 
 
@@ -46,23 +90,23 @@ const template = document.getElementById('friend-template') as HTMLTemplateEleme
 function addFriendToList(name: string) {
 	if (template)
 	{
-    	const friendClone = template.content.cloneNode(true) as DocumentFragment;
+		const friendClone = template.content.cloneNode(true) as DocumentFragment;
 
 		const span = friendClone.querySelector('span');
 
-        // Check if the span element inside the template exists
-        if (span) {
-            span.textContent = name;
-        }
+		// Check if the span element inside the template exists
+		if (span) {
+			span.textContent = name;
+		}
 
-        if (friendListContainer) {
-            friendListContainer.appendChild(friendClone);
+		if (friendListContainer) {
+			friendListContainer.appendChild(friendClone);
 		}
 	}
 }
 
 friends.forEach(friend => {
-    addFriendToList(friend);
+	addFriendToList(friend);
 });
 
 
@@ -75,21 +119,21 @@ const templateBlocked = document.getElementById('blocked-user-template') as HTML
 function addBlockToList(name: string) {
 	if (templateBlocked)
 	{
-    	const Clone = templateBlocked.content.cloneNode(true) as DocumentFragment;
+		const Clone = templateBlocked.content.cloneNode(true) as DocumentFragment;
 
 		const span = Clone.querySelector('span');
 
-        // Check if the span element inside the template exists
-        if (span) {
-            span.textContent = name;
-        }
+		// Check if the span element inside the template exists
+		if (span) {
+			span.textContent = name;
+		}
 
-        if (blockedListContainer) {
-            blockedListContainer.appendChild(Clone);
+		if (blockedListContainer) {
+			blockedListContainer.appendChild(Clone);
 		}
 	}
 }
 
 blockedUsers.forEach(user => {
-    addBlockToList(user);
+	addBlockToList(user);
 });
