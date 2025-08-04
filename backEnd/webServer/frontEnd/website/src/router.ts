65c1b0babe1b08f578b547eafa82f0da60567cf6
dlitran <dariolitran@gmail.com>
2025-08-04 18:36:22 +0200

    fix conflict rebase

--- backEnd/webServer/frontEnd/website/src/router.ts
+++ backEnd/webServer/frontEnd/website/src/router.ts
@@ -3,8 +3,8 @@ import { PongTournament } from './pongTournament.js';
 import {createWebSocket, closeWebSocket} from './websocket.js';
 
 console.log('SPA loaded');
-//history.pushState(null, '', window.location.href);
-customPushState(null, '', window.location.href);
+history.pushState(null, '', window.location.href);
+//customPushState(null, '', window.location.href);
 
 interface Page {
 	path: string;
@@ -24,7 +24,7 @@ const routes: Page[] = [
 				if (response.ok)
 				{
 					let data: string = await response.text();
-					console.log("html:", data);
+					//console.log("html:", data);
 					const root = document.getElementById('root');
 					if (root) {
 						root.innerHTML = data;
@@ -65,7 +65,7 @@ const routes: Page[] = [
 				if (response.ok)
 				{
 					let data: string = await response.text();
-					console.log("html:", data);
+					//console.log("html:", data);
 					const root = document.getElementById('root');
 					if (root) {
 						root.innerHTML = data;
@@ -73,6 +73,43 @@ const routes: Page[] = [
 						newGame.start()
 					} else {
 						console.error('Root element not found');
+					}
+				}
+				else {
+					console.log("Fetch failed.");
+				}
+			}
+			catch (error: unknown)
+			{
+				if (error instanceof Error)
+				{
+					console.error("Error:", error.message);
+				}
+				else
+				{
+					console.error("Unknown error.");
+				}
+			}
+		}
+	},
+
+	{
+		path: "/gameFrame",
+		view: async () => {
+			try {
+				let response: Response = await fetch("/components/game", {
+					method: 'GET',
+					credentials: 'include'
+				});
+				if (response.ok)
+				{
+					let data: string = await response.text();
+					//console.log("html:", data);
+					const gameFrame = document.getElementById('gameFrame');
+					if (gameFrame) {
+						gameFrame.innerHTML = data;
+					} else {
+						console.error('gameFrame element not found');
 					}
 				}
 				else {
@@ -104,7 +141,7 @@ const routes: Page[] = [
 				if (response.ok)
 				{
 					let data: string = await response.text();
-					console.log("html:", data);
+					//console.log("html:", data);
 					const root = document.getElementById('root');
 					if (root) {
 						root.innerHTML = data;
@@ -140,7 +177,7 @@ const routes: Page[] = [
 					method: 'GET',
 					credentials: 'include'
 				});
-				console.log("/home request sent.");
+				//console.log("/home request sent.");
 				if (response.ok)
 				{
 					let data: string = await response.text();
@@ -188,9 +225,9 @@ const routes: Page[] = [
 					root.innerHTML = data;
 
 				const existingScript = document.querySelector("script[src*='/dist/login.js']");
-				console.log(existingScript);
+				//console.log(existingScript);
 				if (existingScript) {
-					console.log('Found and removed existing script:', existingScript);
+					//console.log('Found and removed existing script:', existingScript);
 					existingScript.remove();
 				}
 
@@ -271,7 +308,7 @@ const routes: Page[] = [
 				for (const c of cookies)
 				{
 					const [key, value] = c.trim().split('=');
-					console.log("key-value: ", key, value);
+					//console.log("key-value: ", key, value);
 				}
 				if (root) {
 					root.innerHTML = data;
@@ -358,11 +395,11 @@ const routes: Page[] = [
 				closeWebSocket();
 
 				//window.location.href="/login";
-				console.log("go login");
+				//console.log("go login");
 				history.pushState(null, '', "/login");
-				console.log("before router");
+				//console.log("before router");
 				router();
-				console.log("After router");
+				//console.log("After router");
 			}
 			catch (error: unknown)
 			{
@@ -384,7 +421,7 @@ export const router = async () => {
 	const match = routes.find(route => route.path == path);
 	if (match)
 	{
-		console.log("match");
+		console.log("match: ", match);
 		await match.view();
 		console.log("finished");
 	}
@@ -396,16 +433,17 @@ export const router = async () => {
 
 document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('click', e => {
-	console.log(">>>>>>>>>>>>> click listener called");
+	//console.log(">>>>>>>>>>>>> click listener called");
 	const target = e.target as HTMLElement;
 	if (target.matches('.nav-link')) {
-		console.log(">>>>>>>>>>>>> nav-link found");
+		//console.log(">>>>>>>>>>>>> nav-link found");
 		e.preventDefault();
 		const href = target.getAttribute('data-path')!;
 
-		//history.pushState(null, '', href);
-		customPushState(null, '', href);
-
+		history.pushState(null, '', href);
+		console.log("href: ", href);
+		//customPushState(null, '', href);
+		
 		router();
 	}
 });
@@ -425,13 +463,27 @@ export function customPushState(state: any, title: string, url: string) {
 	console.log("custom push state called");
 }
 
-window.addEventListener('load', () => {
-	const stack = JSON.parse(sessionStorage.getItem('myHistoryStack') || '[]');
-	console.log("page has been reloaded");
-	// Optionally replay the history to simulate back/forward
-	for (let item of stack) {
-	  history.pushState(item.state, item.title, item.url);
-	  console.log("previous routes: ", item.url);
-	}
-	router();
-  });
+// window.addEventListener('load', () => {
+// 	// const stack = JSON.parse(sessionStorage.getItem('myHistoryStack') || '[]');
+// 	// console.log("page has been reloaded");
+// 	// // Optionally replay the history to simulate back/forward
+// 	// if (stack.length > 0) {
+
+// 	// // for (let item of stack) {
+// 	// //   history.pushState(item.state, item.title || '', item.url);
+// 	// //   console.log("previous routes: ", item.url, item.state, item.title);
+// 	// // }
+// 	// //history.replaceState(null, '', window.location.href);
+// 	// const first = stack[0];
+// 	// console.log("stack length: ", stack.length);
+// 	// //history.replaceState(first.state, first.title || '', first.url);
+// 	// for (let i = 0; i < stack.length - 1; i++) {
+// 	// 	const item = stack[i];
+// 	// 	console.log("state: ", item.state, item.title, item.url);
+// 	// 	history.pushState(item.state, item.title || '', item.url);
+// 	//   }
+// 	//   history.replaceState(first.state, first.title || '', first.url);
+// 	//   //history.pushState(stack[0].state, stack[0].title || '', stack[0].url);
+// 	// }
+// 	router();
+//   });
